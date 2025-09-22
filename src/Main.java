55b24429315a6de247424747acaf6b52379250d2
S-Anderson2543 <ander4s7@mail.uc.edu>
2025-09-22 13:55:55 -0400

    Second Commit - Added Code and debugged

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-
+    int numToExamine = 2;
+    int evenCheck = numToExamine % 2;
+    boolean isEven;
+    if (numToExamine % 2 == 0) {
+        isEven = true;
+    }
+    else {
+        isEven = false;
+    }
+    if (isEven == true) {
+        System.out.println("Your number is even");
+    }
+    else {
+        System.out.println("Your number is odd");
+    }
 }
